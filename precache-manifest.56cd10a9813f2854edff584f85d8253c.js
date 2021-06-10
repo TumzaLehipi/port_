aaa265ae0024f7fca197521bbf3c82fc8964e31c
tumzalehipi <brianmoipolai1@gmail.com>
2021-06-10 16:20:07 +0200

    Updates

---
+++ precache-manifest.56cd10a9813f2854edff584f85d8253c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd2c15ac72c6ec0e200da1df18b89d5",
+    "url": "/port/index.html"
+  },
+  {
+    "revision": "035e1f8ed5d50ba6534b",
+    "url": "/port/static/js/2.b244c50e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/port/static/js/2.b244c50e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aecc07516cb18d752a6b",
+    "url": "/port/static/js/main.e9732796.chunk.js"
+  },
+  {
+    "revision": "6dcce418cbb2145e245c",
+    "url": "/port/static/js/runtime-main.49a34994.js"
+  },
+  {
+    "revision": "3dbc4fe5a44cd2134659b257474df5f2",
+    "url": "/port/static/media/avatar.3dbc4fe5.jpg"
+  },
+  {
+    "revision": "11a706e8bf04f97366f1c7d6f1dcfd3f",
+    "url": "/port/static/media/facebook.11a706e8.svg"
+  },
+  {
+    "revision": "6ed3db481fc0668d4efbe2e1e887b8bd",
+    "url": "/port/static/media/github.6ed3db48.svg"
+  },
+  {
+    "revision": "201afcb070acc5707cf61ffba66865d9",
+    "url": "/port/static/media/instagram.201afcb0.svg"
+  },
+  {
+    "revision": "083538a628e5ba64621ed9d3547077e2",
+    "url": "/port/static/media/linkedin.083538a6.svg"
+  }
+]);
