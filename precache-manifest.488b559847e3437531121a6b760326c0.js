e845719884b852d19e19e171ca507edce2167847
tumzalehipi <brianmoipolai1@gmail.com>
2022-05-13 23:46:06 +0200

    Deploy React app to GitHub Pages

---
+++ precache-manifest.488b559847e3437531121a6b760326c0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a793a03887b1b87091adf2e8dc17c94",
+    "url": "/port_/index.html"
+  },
+  {
+    "revision": "c4b3260af34d7e00564e",
+    "url": "/port_/static/js/2.5ff1858f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/port_/static/js/2.5ff1858f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa5f9e7a8511ed0395d",
+    "url": "/port_/static/js/main.1bca1919.chunk.js"
+  },
+  {
+    "revision": "e81a66c388ca70ba529d",
+    "url": "/port_/static/js/runtime-main.8b62d5fa.js"
+  },
+  {
+    "revision": "3dbc4fe5a44cd2134659b257474df5f2",
+    "url": "/port_/static/media/avatar.3dbc4fe5.jpg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/port_/static/media/bars.b3f49afd.svg"
+  },
+  {
+    "revision": "09511483bd8390e42736ee78411339f4",
+    "url": "/port_/static/media/facebook.09511483.svg"
+  },
+  {
+    "revision": "51a0c2d527d3a198d10766ad05ca472b",
+    "url": "/port_/static/media/github.51a0c2d5.svg"
+  },
+  {
+    "revision": "a2581432a9739f754c15e8225630f4e4",
+    "url": "/port_/static/media/instagram.a2581432.svg"
+  },
+  {
+    "revision": "0847cd97a934382c8c34a040c6ef2891",
+    "url": "/port_/static/media/linkedin.0847cd97.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/port_/static/media/times.2c5552a1.svg"
+  }
+]);
